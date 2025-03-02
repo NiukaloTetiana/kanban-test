919fe5b912e9d2f86ff70c43f51cb1da62743a71
Niukalo Tetiana <tanusha.17121994@gmail.com>
2025-03-02 03:10:14 +0200

    add fonts? favicon and container

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import ReactDOM from "react-dom/client";
 
+import { Provider } from "./components/ui/provider";
+
 import { App } from "./components";
 
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <>
-    <App />
-  </>
+  <BrowserRouter>
+    <Provider>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
