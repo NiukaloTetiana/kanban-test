6e0052e624569a7672a6dce8f2907aecfafcddc5
Niukalo Tetiana <tanusha.17121994@gmail.com>
2025-03-08 23:32:40 +0200

    add reorderIssues and moveIssue reducers

--- src/redux/issues/issuesSlice.ts
+++ src/redux/issues/issuesSlice.ts
@@ -20,7 +20,39 @@ const initialState: IIssuesSlice = {
 const issuesSlice = createSlice({
   name: "issues",
   initialState,
-  reducers: {},
+  reducers: {
+    reorderIssues: (state, action) => {
+      const { columnId, sourceIndex, destinationIndex } = action.payload;
+
+      if (!(columnId in state.issues)) return;
+      const column = state.issues[columnId as keyof typeof state.issues];
+      const updatedIssues = [...column];
+      const [movedItem] = updatedIssues.splice(sourceIndex, 1);
+      updatedIssues.splice(destinationIndex, 0, movedItem);
+
+      state.issues[columnId as keyof typeof state.issues] = updatedIssues;
+    },
+    moveIssue: (state, action) => {
+      const { source, destination } = action.payload;
+      if (!destination) return;
+
+      const sourceColumnKey = source.droppableId as keyof typeof state.issues;
+      const destColumnKey =
+        destination.droppableId as keyof typeof state.issues;
+
+      if (
+        !(sourceColumnKey in state.issues) ||
+        !(destColumnKey in state.issues)
+      )
+        return;
+
+      const sourceColumn = state.issues[sourceColumnKey];
+      const destColumn = state.issues[destColumnKey];
+      const [movedIssue] = sourceColumn.splice(source.index, 1);
+
+      destColumn.splice(destination.index, 0, movedIssue);
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(getIssues.fulfilled, (state, action) => {
@@ -53,3 +85,4 @@ const issuesSlice = createSlice({
 export const issuesReducer = issuesSlice.reducer;
 export const { selectToDo, selectInProgress, selectDone, selectIsLoading } =
   issuesSlice.selectors;
+export const { reorderIssues, moveIssue } = issuesSlice.actions;
